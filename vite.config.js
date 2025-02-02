274983f43ea2293e7471356bd61180270966fdbb
letonzt <letonzt@gmail.com>
2025-02-03 00:21:17 +0100

    Fixed vite.config.js for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,57 @@
+// import { defineConfig } from 'vite';
+// import { glob } from 'glob';
+// import injectHTML from 'vite-plugin-html-inject';
+// import FullReload from 'vite-plugin-full-reload';
+// import SortCss from 'postcss-sort-media-queries';
+
+// export default defineConfig(({ command }) => {
+//   return {
+//     define: {
+//       [command === 'serve' ? 'global' : '_global']: {},
+//     },
+//     root: 'src',
+//     build: {
+//       sourcemap: true,
+//       rollupOptions: {
+//         input: glob.sync('./src/*.html'),
+//         output: {
+//           manualChunks(id) {
+//             if (id.includes('node_modules')) {
+//               return 'vendor';
+//             }
+//           },
+//           entryFileNames: chunkInfo => {
+//             if (chunkInfo.name === 'commonHelpers') {
+//               return 'commonHelpers.js';
+//             }
+//             return '[name].js';
+//           },
+//           assetFileNames: assetInfo => {
+//             if (assetInfo.name && assetInfo.name.endsWith('.html')) {
+//               return '[name].[ext]';
+//             }
+//             return 'assets/[name]-[hash][extname]';
+//           },
+//         },
+//       },
+//       outDir: '../dist',
+//       emptyOutDir: true,
+//     },
+//     plugins: [
+//       injectHTML(),
+//       FullReload(['./src/**/**.html']),
+//       SortCss({
+//         sort: 'mobile-first',
+//       }),
+//     ],
+//   };
+// });
+
+
+
+// ---------------------------------
+
+
 import { defineConfig } from 'vite';
 import { glob } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
@@ -10,6 +64,7 @@ export default defineConfig(({ command }) => {
       [command === 'serve' ? 'global' : '_global']: {},
     },
     root: 'src',
+    base: '/goit-js-hw-09/', // 🔥 Укажи имя репозитория на GitHub
     build: {
       sourcemap: true,
       rollupOptions: {
@@ -46,3 +101,4 @@ export default defineConfig(({ command }) => {
     ],
   };
 });
+
